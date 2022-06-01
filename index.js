18c682ff2168888267c09a1845b775b689786fac
nabr <100725601+nabr2730@users.noreply.github.com>
2022-06-01 13:11:11 +0900

    remove(おみくじ): ふざけた選択肢を削除

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ client.on('messageCreate', async message => {
         break;
 
         case 'omikuji':
-            let arr = ["大吉", "中吉", "小吉", "吉", "凶", "大凶", "ぶおおお", "ぶおおおおおおお"];
+            let arr = ["大吉", "中吉", "小吉", "吉", "凶", "大凶"];
             var random = Math.floor(Math.random() * arr.length);
             var result = arr[random];
             message.reply({content: result});
