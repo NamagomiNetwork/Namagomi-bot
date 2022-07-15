f58d0dd7dbf6c1175304d6a05cff10ef1d9ace07
nabr <100725601+nabr2730@users.noreply.github.com>
2022-07-15 14:30:03 +0900

    ログの出力を最適化する

--- bot.js
+++ bot.js
@@ -1,28 +1,33 @@
 const logger = require('./src/modules/logger')
-logger.debug("Starting Logger.... Done!")
-logger.debug('Starting System...')
+logger.info('Botを起動中です...')
 
 // モジュールの読み込み
 const { Client, Intents, Collection, MessageEmbed} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_VOICE_STATES] });
 const fs = require("fs");
 client.commands = new Collection();
-logger.debug("module loading... Done!");
+logger.info("外部モジュールの読み込みが完了しました");
 
-// configあるかたしかめる
-if( fs.existsSync("./configs/config.json") ){
-  logger.debug( "configが設定されています...");
-}else{
+// configあるときだけ読み込む
+if(!fs.existsSync("./configs/config.json") ){
       logger.error("configファイルがありません (config not found) \n ./configs/sample_config.jsonをもとに ./configs/config.json を作成してください")
       logger.error("Error: ENOENT: no such file or directory, open configs/config.json")
       return;
 }
 
 const config = require('./src/utils/get-config.js');
-logger.debug("config Load ... Done!")
+logger.info("configの読み込みに成功しました")
+
+// ログをデバッグモード時には細かいものを出力する
+function debug_logger(msg){
+  if(config.debug.enable.includes("true")){
+    logger.debug(msg)
+  }
+}
 
 // mongodbに接続
-logger.debug("Connected to mongodb")
+debug_logger("Connected to mongodb")
+logger.info("データベースへの接続を開始します")
 require('./src/utils/database')
 
 // ファイルの読み込み
@@ -31,11 +36,9 @@ for (const file of events) {
   const eventName = file.split(".")[0];
   const event = require(`./src/events/${file}`);
   client.on(eventName, event.bind(null, client));
-  if(config.debug.enable.includes("true")){
-    logger.debug(`Loading Event: ${eventName}`);
-  }
+  debug_logger("Loading Event: " + eventName)
 }
-logger.debug("Loading event... Done!")
+logger.info("イベントの読み込みに成功しました")
 
 const commands = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js"));
 for (const file of commands) {
@@ -43,15 +46,12 @@ for (const file of commands) {
   const command = require(`./src/commands/${file}`);
 
   client.commands.set(commandName, command);
-  if(config.debug.enable.includes("true")){
-    logger.debug(`Loading command: ${commandName}`);
-  }
+  debug_logger("Loading command: " + commandName)
 }
-logger.debug("Loading command... Done!")
+logger.info("コマンドの読み込みに成功しました")
 
 // Discord login
 client.login( config.bot.token).catch(err => logger.error(err));
 
-logger.debug('Starting System... Done!')
 // ログを表示
 require("./src/modules/info-logger")
--- src/utils/database.js
+++ src/utils/database.js
@@ -2,11 +2,7 @@ const logger = require('../modules/logger')
 const mongoose = require('mongoose');
 const config = require('./get-config');
 
-mongoose 
-    .connect(config.mongodb.url).then(() => {
-        logger.info("mongodbの接続に成功しました")
-        logger.debug("Connected to mongodb ... Done!")
-    })
+    mongoose.connect(config.mongodb.url)
     .catch((error) => {
         logger.error("mongodbの接続中にエラーが発生しました...")
         logger.error(error);
