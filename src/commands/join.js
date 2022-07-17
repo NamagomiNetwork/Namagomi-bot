d49fa7464953699490fcaeda10dc93e3bd6f307c
nabr <100725601+nabr2730@users.noreply.github.com>
2022-07-18 00:28:54 +0900

    fix: log4jsのconfigが一部環境で動かないのを修正

---
+++ src/commands/join.js
@@ -0,0 +1,23 @@
+const logger = require('../modules/logger')
+const config = require('../utils/get-config');
+const check_admin = require('../utils/check-admin')
+const profileModel = require('../utils/Schema/ProfileSchema');
+const { MessageEmbed } = require('discord.js');
+const err_embed = require('../utils/error-embed')
+
+exports.run = (client, message, args) => {
+    try{
+        
+    } catch (err) {
+            logger.error("コマンド実行エラーが発生しました")
+            logger.error(err)
+            message.channel.send(({embeds: [err_embed.main]}))
+            if(config.debug.enable.includes("true")){
+                message.channel.send(({embeds: [err_embed.debug]}))
+                message.channel.send("エラー内容: ")
+                message.channel.send("```\n"+ err + "\n```")
+            }
+    }
+}
+
+exports.name = "join";
