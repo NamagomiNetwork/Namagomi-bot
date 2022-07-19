4b618ed0595a7fe2106645a6f9f5941ed2e0c830
nabr <100725601+nabr2730@users.noreply.github.com>
2022-07-19 11:37:35 +0900

    feat: 実績通知機能を実装

--- src/sub-systems/seichi-achievement/events/messageCreate.js
+++
@@ -1,8 +0,0 @@
-const config = require('../../../utils/get-config');
-const { MessageEmbed } = require('discord.js');
-const logger = require('../../../modules/logger')
-logger.debug("Load message Create Event")
-module.exports = (client, message) => {
-    if (message.author.bot) return;
-
-  };
