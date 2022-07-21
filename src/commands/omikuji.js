672c807333ddc3a24af5e2495cba58810d218748
nabr <100725601+nabr2730@users.noreply.github.com>
2022-07-21 22:25:57 +0900

    feat: おみくじをいろいろ強化

--- src/commands/omikuji.js
+++ src/commands/omikuji.js
@@ -1,10 +1,9 @@
 const logger = require('../modules/logger')
-const err_embed = require('../utils/error-embed')
-
+const err_embed = require('../utils/error-embed');
+const OmikujiModel = require('../utils/Schema/OmikujiSchema')
+const { MessageEmbed } = require('discord.js');
 exports.run = (client, message, args) => {
-    try{
-
-        
+    try{       
         function bukkubukku(){
         let arr = ["ﾌﾞｯｸﾌﾞｯｸ", "本が溺れた。ﾌﾞｯｸﾌﾞｯｸｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ"];
         var random = Math.floor(Math.random() * arr.length);
@@ -24,10 +23,67 @@ exports.run = (client, message, args) => {
             return;
         }
         
-        let arr = ["大吉", "中吉", "小吉", "吉", "凶", "大凶", "ﾌﾞｯｸﾌﾞｯｸ"];
-        var random = Math.floor(Math.random() * arr.length);
-        var result = arr[random];
-        message.channel.send({content: result});
+        async function run() {
+            const OmikujiData = await OmikujiModel.findOne({ _id: message.author.id });
+            if (!OmikujiData) {
+                logger.error("ユーザー名: " + message.author.username + " ユーザーID: " + message.author.id + "のおみくじプロファイルが見つかりませんでした発生しました...")
+                message.channel.send(({embeds: [err_embed.main]}))
+                return;
+            }
+            if(OmikujiData.one_day_omikuji_feature.includes("true")){
+                if(OmikujiData.one_day_omikuji.includes("true")){
+                    var sudeni_1day_true = new MessageEmbed({
+                        title: "1日1おみくじ",
+                        description: "すでに1日1おみくじを実行しています",
+                        color: 5301186,
+                        "footer": {
+                            "text": "ぶひ"
+                        },
+                        fields: [
+                            {
+                                name: "この機能を無効化するには",
+                                value: "g!one-day-kuji コマンドを実行してください"
+                            },
+                        ]
+                    })
+                    message.channel.send({embeds: [sudeni_1day_true]})
+                    return;
+                }
+            }
+            let arr = ["大吉", "中吉", "小吉", "吉", "凶", "大凶", "ﾌﾞｯｸﾌﾞｯｸ", "ぶひ吉", "ちょうだいきち", "ちょうだいきょう", "ﾌｸﾞｩ🐡"];
+            var random = Math.floor(Math.random() * arr.length);
+            var result = arr[random];
+
+            var maeno_data = OmikujiData.mae_no_omikuji_kekka
+            var success = new MessageEmbed({
+                title: "おみくじ",
+                description: "おみくじをしたよ～",
+                color: 5301186,
+                "footer": {
+                    "text": "ぶひ"
+                },
+                fields: [
+                    {
+                        name: "結果: ",
+                        value: result
+                    },
+                    {
+                        name: "前回の結果: ",
+                        value: maeno_data
+                    },
+                ]
+            })
+            message.channel.send({embeds: [success]})
+            if(OmikujiData.one_day_omikuji_feature.includes("true")){
+                await OmikujiData.updateOne({
+                    one_day_omikuji: true,
+                })
+            }
+            await OmikujiData.updateOne({
+                mae_no_omikuji_kekka: result,
+            })
+        }
+        run()
         } catch (err) {
             logger.error("コマンド実行エラーが発生しました")
             logger.error(err)
