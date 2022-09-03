f33188d3c655233755b8b6a1dde89952d27007ed
nabr <100725601+nabr2730@users.noreply.github.com>
2022-09-03 16:49:57 +0900

    fix: if分の場所を修正

--- src/sub-systems/message-reply.js
+++ src/sub-systems/message-reply.js
@@ -1,8 +1,5 @@
 const TawasiModel = require('../utils/Schema/TawasiSchema');
 module.exports = async (message) => {
-    if (tawasiData.one_day_tawasi_feature.includes("false")) {
-        return;
-    } else {
         // たわしさん
         const tawasiData = await TawasiModel.findOne({ _id: message.author.id });
         if (!tawasiData) {
@@ -14,15 +11,17 @@ module.exports = async (message) => {
                 if (tawasiData.tawasi.includes("true")) {
                     return;
                 }
+                if (tawasiData.one_day_tawasi_feature.includes("false")) {
+                    return;
+                }
                 message.channel.send("https://i.gyazo.com/90c929eccbec4f36d4b15be295660dce.jpg");
                 await tawasiData.updateOne({
                     tawasi: true,
                 })
             }
         }
-    }
         // 豚
         if (message.content.includes('とってもおいしい豚さん')) {
             message.channel.send("https://i.gyazo.com/2408edaa5c00321c1d726cbae8429bdd.jpg");
         }
-}
+    }
