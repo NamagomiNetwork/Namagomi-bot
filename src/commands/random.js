b6bc44d660237eadfaad608de8d6c814b4d971f3
ikazuchizero <90844161+ikazuchizero@users.noreply.github.com>
2022-09-19 21:02:46 +0900

    a

--- src/commands/random.js
+++ src/commands/random.js
@@ -1,36 +1,50 @@
 const logger = require('../modules/logger')
 const err_embed = require('../utils/error-embed')
+const { MessageEmbed } = require('discord.js')
 
 exports.run = (client, message, args) => {
         try{
             const [count, ...choices] = args
             if (!count) 
                 return message.channel.send({content: '試行回数を指定してください'})
-            if (choices.length < 2 || title > choices.length)
+            if (choices.length < 2 || count > choices.length)
                 return message.channel.send({content: `選択肢は最低2つ以上,試行回数の選択肢以下で指定してください`})
+            var arr = choices.join()
 
             //一回目
             var random = Math.floor(Math.random() * choices.length)
-            var result = count[random]
+            var result = choices[random]
             choices.splice(random,1)
             
             //二回目以降
             for (let i=1; i < count; i++){
                 var random = Math.floor(Math.random() * choices.length)
-                var result = result + "," + choices[random]
+                result = result + "," + choices[random]
                 choices.splice(random,1)
             }
 
-            message.channel.send({content: result});
+            var success = new MessageEmbed({
+                title: "抽選結果",
+                description: result,
+                color: 5301186,
+                fields: [
+                    {
+                        name:  "選択数 :" + count,
+                        value:  "選択数 :" + arr
+                    },
+                ]
+            })
+            message.channel.send({embeds: [success]})
+
             } catch (err) {
-                    logger.error("コマンド実行エラーが発生しました")
-                    logger.error(err)
-                    message.channel.send(({embeds: [err_embed.main]}))
-                    if(config.debug.enable.includes("true")){
-                        message.channel.send(({embeds: [err_embed.debug]}))
-                        message.channel.send("エラー内容: ")
-                        message.channel.send("```\n"+ err + "\n```")
-                    }
+                logger.error("コマンド実行エラーが発生しました")
+                logger.error(err)
+                message.channel.send(({embeds: [err_embed.main]}))
+                if(config.debug.enable.includes("true")){
+                    message.channel.send(({embeds: [err_embed.debug]}))
+                    message.channel.send("エラー内容: ")
+                    message.channel.send("```\n"+ err + "\n```")
+                }
             }
 }
 
