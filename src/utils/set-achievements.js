cb4c1ff3c89e2a9e72a89cbc2cbefaf35ef31b28
kuroma6666 <kuroma.creator@gmail.com>
2023-07-02 00:04:20 +0900

    feat:中秋の名月の日付設定

--- src/utils/set-achievements.js
+++ src/utils/set-achievements.js
@@ -1,31 +1,31 @@
+const { MessageEmbed} = require('discord.js');
 module.exports = (client) =>{
 
     const current_year = new Date().getFullYear();
     const fs = require('fs');
-    let path = require('path');
     const config_path = "/../../configs/seichi_achievements.json";
-
     const logger = require("../modules/logger");
     let config = JSON.parse(
         fs.readFileSync(__dirname+config_path,'utf-8')
     );
     console.log(config)
     logger.info(current_year+"年：記念日実績通知用configを更新します。")
-
+    logger.info("=======================")
     set_spring_equinox_day();
-    set_mothersDay();
-    set_fathersDay();
+    set_mothers_day();
+    set_fathers_day();
+    set_harvest_moon();
+    logger.info("=======================")
     write_JSON_file(config);
 
-    function set_mothersDay(){
+    function set_mothers_day(){
         const SUNDAY = 0; //日曜日
         let result_date = get_weekOfDay(current_year,5,2,SUNDAY);
         logger.info("母の日："+result_date);
         config.mothers_day.date = result_date;
     }
 
-    function set_fathersDay()
-    {
+    function set_fathers_day(){
         const SUNDAY = 0; //日曜日
         let result_date = get_weekOfDay(current_year,6,3,SUNDAY);
         logger.info("父の日："+result_date);
@@ -35,27 +35,32 @@ module.exports = (client) =>{
     function set_spring_equinox_day(){
         const month = "3";
         let day = "";
-        
-        if(current_year < 1980 ){
-            day = (current_year >= 1960 && current_year % 4 == 0) ? "3/20":"3/21";
-        }
-        else{
+        if(current_year > 1980 ){
             day = parseInt(20.8431 + 0.242194 * (current_year- 1980)) - parseInt((current_year-1980)/4);
             day.toString();
         }
-        let result_date = month + "/" + day
+        let result_date = month + "/" + day;
         logger.info("春分の日：" + result_date);
         config.spring_equinox_day.date = result_date;
     }
 
     function set_harvest_moon(){
-        //Todo
+        //Ref:https://eco.mtk.nao.ac.jp/koyomi/wiki/C3E6BDA9A4CECCBEB7EEA4C8A4CF.html
+        const array_date = ["9/29","9/17","10/6","9/25","9/15","10/3"];    //2023年~2028年の中秋の名月となる日付
+        const year = 2023;
+        let result_date = "";
+
+        result_date = array_date[current_year-year];
+        logger.info("中秋の名月：" + result_date);
+        config.harvest_moon.date = result_date;
     }   
+
     function write_JSON_file(obj){
         const SPACE = 2 //JSON文字列の整形出力
         fs.writeFileSync(
             (__dirname+config_path),JSON.stringify(obj,null,SPACE)
-        )
+        );
+        logger.info("記念日実績通知用configの更新に成功しました。")
     }
 
     function get_weekOfDay(current_year,month,week,day){
@@ -71,12 +76,8 @@ module.exports = (client) =>{
         let result = new Date(current_year+"/"+month+"/"+day);
         let result_m = parseInt(result.getMonth())+1;
         let result_d = parseInt(result.getDate());
-        result = result_m.toString() +"/"+result_d.toString();
+        result = result_m.toString() + "/" + result_d.toString();
         return result;
-    }
-
-    function convert_old2new(){
-        //Todo
     }
 
 }
