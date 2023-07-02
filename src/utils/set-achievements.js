fd429947492df52e86a617bae0dbc0396765c6b5
kuroma6666 <kuroma.creator@gmail.com>
2023-07-02 14:34:24 +0900

    feat:書き込み時エラー処理が無かったので追加・styleも若干修正

--- src/utils/set-achievements.js
+++ src/utils/set-achievements.js
@@ -5,31 +5,29 @@ module.exports = (client) =>{
     const fs = require('fs');
     const config_path = "/../../configs/seichi_achievements.json";
     const logger = require("../modules/logger");
-    let config = JSON.parse(
-        fs.readFileSync(__dirname+config_path,'utf-8')
-    );
-    console.log(config)
-    logger.info(current_year+"年：記念日実績通知用configを更新します。")
+    let seichi_achievement = require('../utils/get-achievements')
+
+    logger.info(current_year+"年：記念日実績通知用configを更新します")
     logger.info("=======================")
     set_spring_equinox_day();
     set_mothers_day();
     set_fathers_day();
     set_harvest_moon();
     logger.info("=======================")
-    write_JSON_file(config);
+    write_JSON_file(seichi_achievement);
 
     function set_mothers_day(){
         const SUNDAY = 0; //日曜日
         let result_date = get_weekOfDay(current_year,5,2,SUNDAY);
         logger.info("母の日："+result_date);
-        config.mothers_day.date = result_date;
+        seichi_achievement.mothers_day.date = result_date;
     }
 
     function set_fathers_day(){
         const SUNDAY = 0; //日曜日
         let result_date = get_weekOfDay(current_year,6,3,SUNDAY);
         logger.info("父の日："+result_date);
-        config.fathers_day.date = result_date;
+        seichi_achievement.fathers_day.date = result_date;
     }
 
     function set_spring_equinox_day(){
@@ -41,7 +39,7 @@ module.exports = (client) =>{
         }
         let result_date = month + "/" + day;
         logger.info("春分の日：" + result_date);
-        config.spring_equinox_day.date = result_date;
+        seichi_achievement.spring_equinox_day.date = result_date;
     }
 
     function set_harvest_moon(){
@@ -52,15 +50,26 @@ module.exports = (client) =>{
 
         result_date = array_date[current_year-year];
         logger.info("中秋の名月：" + result_date);
-        config.harvest_moon.date = result_date;
+        seichi_achievement.harvest_moon.date = result_date;
     }   
 
     function write_JSON_file(obj){
-        const SPACE = 2 //JSON文字列の整形出力
-        fs.writeFileSync(
-            (__dirname+config_path),JSON.stringify(obj,null,SPACE)
-        );
-        logger.info("記念日実績通知用configの更新に成功しました。")
+        const SPACE = 2; //JSON文字列の整形出力
+        let result = false;
+        try{
+            fs.writeFileSync(
+                (__dirname+config_path),JSON.stringify(obj,null,SPACE)
+            );
+            result = true;
+        }catch(err){
+            logger.error(err.message);
+            result = false;
+        }
+        if(result){
+            logger.info("記念日実績通知用configの更新に成功しました")
+        } else {
+            logger.error("記念日実績通知用configの更新に失敗しました...")
+        }
     }
 
     function get_weekOfDay(current_year,month,week,day){
