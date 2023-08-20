82f0aec8035003e7dd489283244fbf1bc61b66ae
kuroma6666 <kuroma.creator@gmail.com>
2023-08-20 19:34:17 +0900

    chore:小数第3桁より後は切り捨て

--- src/commands/status.js
+++ src/commands/status.js
@@ -3,7 +3,7 @@ const package = require("../../package.json");
 const os = require("os");
 const logger = require("../modules/logger");
 const err_embed = require("../utils/error-embed");
-
+const mem_
 exports.run = (client, message, args) => {
   try {
     // 空きメモリを計算
@@ -11,6 +11,7 @@ exports.run = (client, message, args) => {
     const freemem_kb = freemem_byte / 1024;
     const freemem_mb = freemem_kb / 1024;
     const freemem_gb = freemem_mb / 1024;
+    const freemem = Math.floor(freemem_gb * 1000) / 1000;
 
     var embed = new MessageEmbed({
       title: "SystemStatus",
@@ -25,7 +26,7 @@ exports.run = (client, message, args) => {
         },
         {
           name: "Free Memory",
-          value: freemem_gb + " GB",
+          value: freemem + " GB",
         },
         {
           name: "bot-version",
