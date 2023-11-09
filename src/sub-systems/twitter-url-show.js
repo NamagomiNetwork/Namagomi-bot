b758fa44de533f9ccd08e3f7169755f40c6367ca
kuroma6666 <kuroma.creator@gmail.com>
2023-11-10 01:09:00 +0900

    feat: post内容を展開する埋め込みパネルを作成した（画像・動画はまだ未対応）

--- src/sub-systems/twitter-url-show.js
+++ src/sub-systems/twitter-url-show.js
@@ -1,11 +1,32 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.x_twitter_com = (client, message) => {
-    const regex = /https:\/\/(twitter\.com|x\.com)\/[A-Za-z0-9]*\/status\/\d+\//;
+    const regex = /https:\/\/(twitter\.com|x\.com)\/[A-Za-z0-9_]*\/status\/(\d+)/;
     const results = message.content.match(regex);
 
     if (!results) return;
-    if (message.author.bot) return;
 
-    console.log(results);
+    let replaceURL = results[0].replace(/twitter.com|x.com/, "api.vxtwitter.com");
+    console.log(results[0]);
+
+    fetch(replaceURL)
+        .then((res) => res.json())
+        .then((post) => {
+            const msgpanel = new MessageEmbed()
+                .setDescription(`${post.text}`)
+                .setAuthor({
+                    name: `${post.user_name}` + `(@` + `${post.user_screen_name}` + `)`,
+                })
+                .setURL(post.tweetURL)
+                .setDescription(post.text + `\n\n[Xで表示する](` + post.tweetURL + `)`)
+                .setTimestamp(post.date)
+                .setFooter({
+                    text: `${post.user_screen_name}`,
+                    iconURL: `https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png`,
+                });
+            if (post.media_extend) {
+                msgpanel.setImage(`${post.mediaURLs.map((media_extend) => media_extend.thumbnail_url)}`);
+            }
+            message.channel.send({ embeds: [msgpanel] });
+        });
 };
