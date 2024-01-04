9dd2354537774f7683ee125c0ef075c71bdf89c6
kuroma6666 <kuroma.creator@gmail.com>
2024-01-04 14:42:22 +0900

    add: カラーコード定数を追加

---
+++ src/utils/color-code.js
@@ -0,0 +1,14 @@
+const NOTIFY_COLOR = 0x4a90e2; //強い青紫系の色(4886754)
+const ATTENTION_COLOR = 0xfd5267; //明るい赤系の色(16601703)
+const ERROR_COLOR = 0xfd5267; //明るい赤系の色(16601703)
+const DETAIL_COLOR = 0xffff12; //あざやかな黄系の色(16776978)
+const BLOCKED_NOTIFY_COLOR = 0x3acad6; //強い青系の色(3853014)
+
+const CMD_RUN_COLOR = 0x50e3c2; //強い青緑系の色(5301186)
+const CMD_NOTFOUND_COLOR = 0xfd5267; //明るい赤系の色(16601703)
+
+// region export color-code constant
+
+// endregion
+
+// ref:HTMLカラーコード: WEB色見本 原色大辞典(https://www.colordic.org/)
