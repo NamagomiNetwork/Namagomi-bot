d53d09e636f4b480c0a79db02cd405fe69cdbdc7
kuroma6666 <kuroma.creator@gmail.com>
2024-02-09 23:59:30 +0900

    fix: 意味合いがおかしい気がするので、ファイル名・スキーマ/モデル名を変更

--- src/utils/Schema/PostExpansionSettingsSchema.js
+++ src/utils/Schema/PostExpansionSettingsSchema.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const URLShowSettingsSchema = new mongoose.Schema({
+const PostExpansionSettingsSchema = new mongoose.Schema({
     _id: { type: String }, //ユーザーID
     x_twitter_show: { type: String }, //Twitterのポストを埋め込み展開するかどうか(true or false)
     discord_show: { type: String }, //Discordのポストを埋め込み展開するかどうか (true or false)（★未使用）
 });
 
-const model = mongoose.model('URLShowSettings', URLShowSettingsSchema);
+const model = mongoose.model("PostExpansionSettings", PostExpansionSettingsSchema);
 
 module.exports = model;
