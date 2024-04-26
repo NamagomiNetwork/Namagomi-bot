d5ed22adf7b905e252a66ba949951b4efe43e27e
ikazuchizero <90844161+ikazuchizero@users.noreply.github.com>
2024-04-26 11:50:24 +0900

    らんだむをかえたよ

---
+++ random.js
@@ -0,0 +1,37 @@
+const logger = require("../modules/logger");
+const err_embed = require("../utils/error-embed");
+const { MessageEmbed } = require("discord.js");
+
+exports.run = (client, message, args) => {
+  try {
+    const [...choices] = args;
+    let arr = choices.join();
+    
+    const random = Math.floor(Math.random() * choices.length);
+    const result = choices[random];
+
+    const success = new MessageEmbed({
+      title: "抽選結果",
+      description: result,
+      color: 5301186,
+      fields: [
+        {
+          name: "選択肢:",
+          value: arr,
+        },
+      ],
+    });
+    message.channel.send({ embeds: [success] });
+  } catch (err) {
+    logger.error("コマンド実行エラーが発生しました");
+    logger.error(err);
+    message.channel.send({ embeds: [err_embed.main] });
+    if (config.debug.enable.includes("true")) {
+      message.channel.send({ embeds: [err_embed.debug] });
+      message.channel.send("エラー内容: ");
+      message.channel.send("```\n" + err + "\n```");
+    }
+  }
+};
+
+exports.name = "random";
