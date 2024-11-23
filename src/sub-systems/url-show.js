eefdef5c838045c85d3cafbd32643d78093e1ab9
kuroma6666 <kuroma.creator@gmail.com>
2024-11-23 23:04:13 +0900

    fix: Discord内メッセージ展開機能の見直し修正

--- src/sub-systems/url-show.js
+++ src/sub-systems/url-show.js
@@ -36,35 +36,51 @@ exports.discord_com = (client, message) => {
     channelch.messages
         .fetch(message_id)
         .then((msg) => {
+            let embeds = [];
             const msgpanel = new EmbedBuilder()
-                .setDescription(`${msg.content}`)
-                .setAuthor({
-                    name: `${msg.author.username}`,
-                    iconURL: msg.author.avatarURL({ dynamic: true }),
-                })
+                .setDescription(msg.content || ' ')
                 .setTimestamp(msg.createdAt)
-                .setFooter({
-                    text: `${msg.channel.name}`,
-                    iconURL: `${
-                        msg.guild.iconURL() == null
-                            ? "https://www.freepnglogos.com/uploads/discord-logo-png/discord-logo-logodownload-download-logotipos-1.png"
-                            : msg.guild.iconURL()
-                    }`,
-                });
 
-            if (msg.attachments) {
-                msgpanel.setImage(`${msg.attachments.map((attachment) => attachment.url)}`);
-            }
+                if (msg.author && msg.author.username) {
+                    msgpanel.setAuthor({
+                        name: msg.author.username,
+                        iconURL: msg.author.avatarURL({ dynamic: true }) || ' ',
+                    });
+                }
 
-            message.channel.send({ embeds: [msgpanel] });
+                if (msg.guild && msg.channel) {
+                    msgpanel.setFooter({
+                        text: msg.channel.name || '不明なチャンネル',
+                        iconURL:
+                            msg.guild.iconURL() ||
+                            'https://www.freepnglogos.com/uploads/discord-logo-png/discord-logo-logodownload-download-logotipos-1.png',
+                    });
+                }
+                embeds.push(msgpanel);
+                if (msg.attachments.size > 0) {
+                    msg.attachments.forEach((attachment) => {
+                        
+                        console.log(attachment);
+                        embeds.push({
+                            url: `${message.content}`,
+                            image: {
+                                url: attachment.url,
+                            },
+                        });
+                    });
+                }
 
+            message.channel.send({ embeds: embeds });
+/*
             if (msg.embeds[0]) {
                 message.channel.send({ embeds: [msg.embeds[0]] });
             }
+                */
         })
         .catch(console.error);
 };
 
+// TODO:discord_comとdiscord_ptb_comで中身が重複していて修正が面倒なので、discord_comに含める
 exports.discord_ptb_com = (client, message) => {
     const re = /https:\/\/ptb\.discord\.com\/channels\/(\d{16,19})\/(\d{16,19})\/(\d{16,19})/;
 
