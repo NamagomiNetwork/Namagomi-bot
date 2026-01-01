49c4c8abc628b15fe67fa058887efca32fb10b38
ikazuchizero <90844161+ikazuchizero@users.noreply.github.com>
2026-01-01 10:35:19 +0900

    omikuji.js を更新

--- src/commands/omikuji.js
+++ src/commands/omikuji.js
@@ -44,7 +44,6 @@ exports.run = async (client, message) => {
         
         //変数宣言
         let result = "";
-        /*
         let unique = false;
         const arrKo = ["や！", "こばわ"];
         const arrButa = ["黙れ豚", "しばくぞ豚"];
@@ -90,9 +89,7 @@ exports.run = async (client, message) => {
             ];
             let randomNum = Math.floor(Math.random() * arr.length);
             result = arr[randomNum];
-            */
 
-            result = "ちょうだいきち"
             let maeno_data = OmikujiData.mae_no_omikuji_kekka;
             let success = new EmbedBuilder({
                 title: "おみくじ",
