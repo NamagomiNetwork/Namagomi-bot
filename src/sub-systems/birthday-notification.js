d1024bbcef276b78d38700212533b68bfde2563e
yuuki1293 <yuuki1293@outlook.jp>
2026-01-03 12:43:39 +0900

    誕生日チャンネルの自動作成、アーカイブルーチンを実装

--- src/sub-systems/birthday-notification.js
+++ src/sub-systems/birthday-notification.js
@@ -1,6 +1,10 @@
-const { EmbedBuilder } = require("discord.js");
+const { ChannelType, EmbedBuilder } = require("discord.js");
 const ProfileModel = require("../utils/Schema/ProfileSchema");
 const color = require("../utils/color-code");
+const config = require("../utils/get-config");
+const sendErrorMessage = require("../modules/error-message");
+
+const birthday_channels = [];
 
 module.exports = async (client) => {
     const now = new Date(
@@ -15,15 +19,98 @@ module.exports = async (client) => {
         birthday_day: day,
     });
 
-    for(const birthdayProfile of birthdaysToday) {
+    await archive_birthday_channel(client);
+
+    for (const birthdayProfile of birthdaysToday) {
+        const channelName = birthdayProfile.name + "たん";
+
+        const mention = `<@&${config.birthday.role}>`;
+
         const embed = new EmbedBuilder({
             title: "誕生日通知",
             color: color.CMD_RUN,
-            description: ":tada:今日は" + "<@" + birthdayProfile._id + ">" + "の誕生日です:tada:",
+            description: `:tada:今日は <@${birthdayProfile._id}>の誕生日です:tada:`,
         });
 
-        // const mention = "<@&" + config.birthday_notification.role + ">";
-        // client.channels.cache.get("1135407155596496937").send(mention);
-        client.channels.cache.get("1135407155596496937").send({ embeds: [embed] });
+        const channel = open_birthday_channel(client, channelName);
+
+        if(!channel) {
+            continue;
+        }
+
+        channel.send(mention);
+        channel.send({ embeds: [embed] });
     };
 }
+
+async function open_birthday_channel(client, channelName) {
+    try {
+        const message = new EmbedBuilder({
+            title: "誕生日ルーチン",
+            description: "誕生日チャンネルの作成に失敗しました",
+            color: color.ERROR,
+        });
+
+        const categoryId = config.birthday.channel_category;
+        if (!categoryId) {
+            throw new Error("channel_category not set.");
+        }
+
+        const category = await client.channels.fetch(categoryId);
+        if (category.type !== ChannelType.GuildCategory) {
+            throw new Error("channel_category must be GuildCategory. " + category.type + "is set.");
+        }
+
+        const channel = await category.guild.channels.create({
+            name: channelName,
+            type: ChannelType.GuildText,
+            parent: category.id,
+        });
+
+        birthday_channels.push(channel)
+
+        return channel;
+    } catch (err) {
+        sendErrorMessage(err, message);
+        return null;
+    }
+}
+
+// 既にある誕生日チャンネルをArchive-birthdayに移動し、書き込み権限を削除する。
+async function archive_birthday_channel(client) {
+    try {
+        const message = new EmbedBuilder({
+            title: "誕生日ルーチン",
+            description: "誕生日チャンネルのアーカイブに失敗しました",
+            color: color.ERROR,
+        });
+
+        const archiveCategoryId = config.birthday.archive_category;
+        if (!archiveCategoryId) {
+            throw new Error("archive_category not set.");
+        }
+
+        const archiveCategory = await client.channels.fetch(archiveCategoryId);
+        if (archiveCategory.type !== ChannelType.GuildCategory) {
+            throw new Error(
+                "archive_category must be GuildCategory. " + archiveCategory.type + " is set."
+            );
+        }
+
+        const channels = await Promise.all(birthday_channels);
+        for (const channel of channels) {
+            if (!channel) {
+                continue;
+            }
+            await channel.setParent(archiveCategory.id);
+            await channel.permissionOverwrites.edit(channel.guild.roles.everyone, {
+                SendMessages: false,
+            });
+        }
+
+        birthday_channels.clear();
+    } catch (err) {
+        sendErrorMessage(err, message);
+    }
+}
+
